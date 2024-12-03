1a9c17fd6abe9e20ada45e2d86a860a94836bd84
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-03 10:25:17 +0530

    Add files via upload

---
+++ comparisionoperator.java
@@ -0,0 +1,14 @@
+public class comparisionoperator {
+    public static void main(String args[])
+    {
+        int a=8,b=10;
+        
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        
+    }
+}

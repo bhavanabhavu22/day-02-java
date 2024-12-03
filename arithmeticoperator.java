5ee307c8c14889eb8126e82dadb060ae470f643e
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-03 10:24:12 +0530

    Add files via upload

---
+++ arithmeticoperator.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+    public class arithmeticoperator
+    {
+        public static void main(String argd[])
+        {
+            int a=5,b=6;
+            System.out.println("a+b");
+            System.out.println("a-b");
+            System.out.println("a*b");
+            System.out.println("a/b");
+            System.out.println("a%b");
+
+        }
+    
+}
